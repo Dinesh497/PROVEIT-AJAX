7bba804d4f921a1dc26c13b032d258e6847dd549
ralphoud <ralph_oud@hotmail.com>
2014-03-24 19:41:03 +0100

    LoginScreen gefixed

---
+++ AjaxAgendaApp/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 24-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *TrainerTextField;
+@property (weak, nonatomic) IBOutlet UITextField *WachtwoordTextField;
+
+@end
