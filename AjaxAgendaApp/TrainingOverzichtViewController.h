15c4105600652bd2fcc05065796a420285724790
ralphoud <ralph_oud@hotmail.com>
2014-03-27 10:29:53 +0100

    Storyboard aangepast aan flow chart

---
+++ AjaxAgendaApp/TrainingOverzichtViewController.h
@@ -0,0 +1,14 @@
+//
+//  TrainingOverzichtViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 27-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainingOverzichtViewController : UIViewController
+- (IBAction)TrainingAfronden:(id)sender;
+
+@end
