1d3dfe7f5972b332e3f87418f468bafda7763157
ralphoud <ralph_oud@hotmail.com>
2014-03-27 10:37:09 +0100

    Classes toegevoegd

---
+++ AjaxAgendaApp/TrainingInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrainingInfoViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 27-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainingInfoViewController : UIViewController
+
+@end
