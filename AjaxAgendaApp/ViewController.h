9f3e65745bb2d2139c538e8c5c7d0b255760e73a
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-04-11 16:09:28 +0200

    Revert d66a6d7..b04cf96
    This rolls back to commit d66a6d7b691600c092a413e3fe8dd50e8f31278c.

--- AjaxAgendaApp/ViewController.h
+++ AjaxAgendaApp/ViewController.h
@@ -1,13 +1,13 @@
 //
-//  MenuViewController.h
+//  ViewController.h
 //  AjaxAgendaApp
 //
-//  Created by Ralph Oud on 26-03-14.
+//  Created by Dinesh Bhagwandin on 24-03-14.
 //  Copyright (c) 2014 Prove IT. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MenuViewController : UIViewController
+@interface ViewController : UIViewController
 
 @end
