c3877f92e53b209ec9c9d28dcc6e95f732ada25d
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-04-11 16:14:46 +0200

    Revert "asd"
    Conflicts:
    
    AjaxAgendaApp.xcodeproj/project.xcworkspace/xcshareddata/AjaxAgendaApp.x
    ccheckout
    
    AjaxAgendaApp.xcodeproj/project.xcworkspace/xcuserdata/ralph_oud.xcuserd
    atad/UserInterfaceState.xcuserstate
    
    AjaxAgendaApp.xcodeproj/xcuserdata/ralph_oud.xcuserdatad/xcdebugger/Brea
    kpoints_v2.xcbkptlist
    	AjaxAgendaApp/Base.lproj/Main.storyboard

---
+++ AjaxAgendaApp/LoginViewController.h
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 24-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *TextView;
+@property (weak, nonatomic) IBOutlet UIImageView *TextViewImage;
+@property (weak, nonatomic) IBOutlet UITextField *TrainerTextField;
+@property (weak, nonatomic) IBOutlet UITextField *WachtwoordTextField;
+- (IBAction)TrainerTextDidExit:(id)sender;
+- (IBAction)WachtwoordTextDidExit:(id)sender;
+
+@end
---
+++ AjaxAgendaApp/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 26-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
