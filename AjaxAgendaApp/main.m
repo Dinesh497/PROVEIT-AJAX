1441a4b7535f359d40415b92f7556e274545ee2a
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-04-11 16:39:04 +0200

    Initial commit from local git

---
+++ AjaxAgendaApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AjaxAgendaApp
+//
+//  Created by Dinesh Bhagwandin on 11-04-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
