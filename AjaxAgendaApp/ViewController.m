428480ac526d665dfad0f6cb21c0eedca2fc84ee
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-04-11 16:23:37 +0200

    Revert "asd"
    This reverts commit 602259aacfcb43726174616d86260c13b5c43d45.

---
+++ AjaxAgendaApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AjaxAgendaApp
+//
+//  Created by Dinesh Bhagwandin on 24-03-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
