02ab5fbd7a8465da15e760aa9cde7584eb50df39
ralphoud <ralph_oud@hotmail.com>
2014-04-13 23:31:35 +0200

    Laatste fixies van de avond

--- AjaxAgendaApp/TrainingInfoViewController.m
+++ AjaxAgendaApp/TrainingInfoViewController.m
@@ -9,10 +9,16 @@
 #import "TrainingInfoViewController.h"
 
 @interface TrainingInfoViewController ()
+
 @property NSArray *HeadersSectionOne;
 @property NSArray *HeadersSectionTwo;
 @property NSArray *DetailsSectionOne;
 @property NSArray *DetailsSectionTwo;
+
+@property NSDate *Date;
+@property NSDate *BeginTime;
+@property NSDate *EndTime;
+
 @end
 
 @implementation TrainingInfoViewController
@@ -30,19 +36,18 @@
 {
     [super viewDidLoad];
     
-    // De tableview bouwen
-    _TrainingTableView.delegate =       self;
-    _TrainingTableView.dataSource =     self;
-    _TrainingTableView.layer.cornerRadius = 10;
+    // De tableview installeren
+    _TrainingTableView.delegate =            self;
+    _TrainingTableView.dataSource =          self;
+    _TrainingTableView.layer.cornerRadius =    10;
     
     // Maak Titels en start details voor tijd
-    _HeadersSectionOne = [[NSArray alloc] initWithObjects:@"Begin",@"Eind", nil];
-    _DetailsSectionOne = [[NSArray alloc] initWithObjects:@"12:00", @"13:00", nil];
+    _HeadersSectionOne = [[NSArray alloc] initWithObjects:@"Datum",     @"Begin",   @"Eind",    nil];
+    _DetailsSectionOne = [[NSArray alloc] initWithObjects:@"Vandaag",   @"12:00",   @"13:00",   nil];
     
     // Maak Titels en start details voor locatie
-    _HeadersSectionTwo = [[NSArray alloc] initWithObjects:@"Veld", nil];
-    _DetailsSectionTwo = [[NSArray alloc] initWithObjects:@"1", nil];
-    
+    _HeadersSectionTwo = [[NSArray alloc] initWithObjects:@"Veld",  nil];
+    _DetailsSectionTwo = [[NSArray alloc] initWithObjects:@"1",     nil];
     
     // Do any additional setup after loading the view.
 }
@@ -65,11 +70,11 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if(section == 0){
-        // De eerste sectie heeft twee rijen nodig voor begin- en eindtijd
-        return 2;
+        // De eerste sectie heeft drie rijen nodig voor begin tijd, eind tijd en datum
+        return [_HeadersSectionOne count];
     } else{
         // De tweede sectie heeft een rij nodig voor het veld
-        return 1;
+        return [_HeadersSectionTwo count];
     }
 }
 
@@ -78,18 +83,19 @@
     
     if (indexPath.section == 0) {
         // Sectie 1: Tijd
-            cell.textLabel.text = [_HeadersSectionOne objectAtIndex:indexPath.row];
-            cell.detailTextLabel.text = [_DetailsSectionOne objectAtIndex:indexPath.row];
+        cell.textLabel.text = [_HeadersSectionOne objectAtIndex:indexPath.row];
+        cell.detailTextLabel.text = [_DetailsSectionOne objectAtIndex:indexPath.row];
     } else{
         // Sectie 2: Locatie
-            cell.textLabel.text = [_HeadersSectionTwo objectAtIndex:indexPath.row];
-            cell.detailTextLabel.text = [_DetailsSectionTwo objectAtIndex:indexPath.row];
+        cell.textLabel.text = [_HeadersSectionTwo objectAtIndex:indexPath.row];
+        cell.detailTextLabel.text = [_DetailsSectionTwo objectAtIndex:indexPath.row];
     }
     
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    // Als er een rij wordt geselecteerd
     [_TrainingTableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
