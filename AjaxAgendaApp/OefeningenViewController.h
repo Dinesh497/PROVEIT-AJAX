7d1e0e04b7a9d725fbedf7be86e00064320e9f22
ralphoud <ralph_oud@hotmail.com>
2014-04-17 19:08:10 +0200

    OefeningView toegevoegd

---
+++ AjaxAgendaApp/OefeningenViewController.h
@@ -0,0 +1,14 @@
+//
+//  OefeningenViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Ralph Oud on 17-04-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OefeningenViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *FormulierView;
+
+@end
