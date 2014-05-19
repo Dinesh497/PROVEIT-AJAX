cd0b2eacad1de8b09269e374144cace2439fc114
DylanBartels <d_bartels@live.nl>
2014-05-19 10:19:49 +0200

    Agenda toegevoegd

---
+++ AjaxAgendaApp/AgendaViewController.h
@@ -0,0 +1,13 @@
+//
+//  AgendaViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Dylan Bartels on 13/05/14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AgendaViewController : UIViewController
+
+@end
---
+++ AjaxAgendaApp/AgendaViewController.m
@@ -0,0 +1,85 @@
+//
+//  AgendaViewController.m
+//  AjaxAgendaApp
+//
+//  Created by Dylan Bartels on 13/05/14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import "AgendaViewController.h"
+#import "DSLCalendarView.h"
+
+
+@interface AgendaViewController () <DSLCalendarViewDelegate>
+
+@property (weak, nonatomic) IBOutlet DSLCalendarView *calendarView;
+
+@end
+
+@implementation AgendaViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark - DSLCalendarViewDelegate methods
+
+- (void)calendarView:(DSLCalendarView *)calendarView didSelectRange:(DSLCalendarRange *)range {
+    if (range != nil) {
+        NSLog( @"Selected %d/%d - %d/%d", range.startDay.day, range.startDay.month, range.endDay.day, range.endDay.month);
+    }
+    else {
+        NSLog( @"No selection" );
+    }
+}
+
+- (DSLCalendarRange*)calendarView:(DSLCalendarView *)calendarView didDragToDay:(NSDateComponents *)day selectingRange:(DSLCalendarRange *)range {
+    if (YES) { // Only select a single day
+        return [[DSLCalendarRange alloc] initWithStartDay:day endDay:day];
+    }
+    else if (NO) { // Don't allow selections before today
+        NSDateComponents *today = [[NSDate date] dslCalendarView_dayWithCalendar:calendarView.visibleMonth.calendar];
+        
+        NSDateComponents *startDate = range.startDay;
+        NSDateComponents *endDate = range.endDay;
+        
+        if ([self day:startDate isBeforeDay:today] && [self day:endDate isBeforeDay:today]) {
+            return nil;
+        }
+        else {
+            if ([self day:startDate isBeforeDay:today]) {
+                startDate = [today copy];
+            }
+            if ([self day:endDate isBeforeDay:today]) {
+                endDate = [today copy];
+            }
+            
+            return [[DSLCalendarRange alloc] initWithStartDay:startDate endDay:endDate];
+        }
+    }
+    
+    return range;
+}
+
+- (void)calendarView:(DSLCalendarView *)calendarView willChangeToVisibleMonth:(NSDateComponents *)month duration:(NSTimeInterval)duration {
+    NSLog(@"Will show %@ in %.3f seconds", month, duration);
+}
+
+- (void)calendarView:(DSLCalendarView *)calendarView didChangeToVisibleMonth:(NSDateComponents *)month {
+    NSLog(@"Now showing %@", month);
+}
+
+- (BOOL)day:(NSDateComponents*)day1 isBeforeDay:(NSDateComponents*)day2 {
+    return ([day1.date compare:day2.date] == NSOrderedAscending);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+
