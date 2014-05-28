d36545c03c7b2d5ce85093483b3af4ebb7c71b05
ralphoud <ralph_oud@hotmail.com>
2014-05-28 14:20:43 +0200

    Fixed the commit

--- AjaxAgendaApp/LoginViewController.m
+++ AjaxAgendaApp/LoginViewController.m
@@ -171,7 +171,6 @@
 
 - (NSString*) PasswordfromDatabase:(NSString*)Trainername{
     // Get password from database from the using Trainer
-<<<<<<< HEAD
     NSString *Password;
     
     const char *dbpath = [_databasePath UTF8String];
@@ -198,28 +197,6 @@
         sqlite3_close(_ajaxtrainingDB);
     }
     NSLog(@"In password array zitten: %@",_passwords);
-=======
-    NSString *Password = Trainername;
-    
-    const char *dbpath = [_databasePath UTF8String];
-    
-    if (sqlite3_open(dbpath, &_ajaxtrainingDB) == SQLITE_OK)
-    {
-    
-        NSString *queryplayersa1_sql = [NSString stringWithFormat:@"Select password from trainers where name = '%@'", Trainername];
-        
-        const char *querya1_stmt = [queryplayersa1_sql UTF8String];
-        char *Errmsg;
-        sqlite3_stmt *statement;
-    
-        sqlite3_exec(_ajaxtrainingDB, querya1_stmt, NULL, NULL, &Errmsg);
-        
-        Password = [NSString stringWithUTF8String:(char *)sqlite3_column_text(statement, 0)];
-        
-        sqlite3_finalize(statement);
-        }
-    sqlite3_close(_ajaxtrainingDB);
->>>>>>> FETCH_HEAD
 
     return Password;
 }
