33bf4a32511a35296c7657c413ae6e828387d699
DylanBartels <d_bartels@live.nl>
2014-07-07 14:29:45 +0200

    herro

---
+++ AjaxAgendaApp/WeergaveViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeergaveViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Dylan Bartels on 02/07/14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeergaveViewController : UIViewController
+
+@end
