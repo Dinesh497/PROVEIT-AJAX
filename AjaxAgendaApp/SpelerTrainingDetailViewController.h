0cfb4b3188267cbe30fc8aeae840e20d9ece09a5
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-07-09 14:49:51 +0200

    YO new view, and trainingenview opnieuw

---
+++ AjaxAgendaApp/SpelerTrainingDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpelerTrainingDetailViewController.h
+//  AjaxAgendaApp
+//
+//  Created by Dinesh Bhagwandin on 09-07-14.
+//  Copyright (c) 2014 Prove IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpelerTrainingDetailViewController : UIViewController
+
+@end
--- AjaxAgendaApp/SpelerTrainingDetailViewController.m
+++ AjaxAgendaApp/SpelerTrainingDetailViewController.m
@@ -1,20 +1,18 @@
 //
-//  TrainingenViewController.m
+//  SpelerTrainingDetailViewController.m
 //  AjaxAgendaApp
 //
-//  Created by Ralph Oud on 04-07-14.
+//  Created by Dinesh Bhagwandin on 09-07-14.
 //  Copyright (c) 2014 Prove IT. All rights reserved.
 //
 
-#import "TrainingenViewController.h"
+#import "SpelerTrainingDetailViewController.h"
 
-@interface TrainingenViewController ()
-
-
+@interface SpelerTrainingDetailViewController ()
 
 @end
 
-@implementation TrainingenViewController
+@implementation SpelerTrainingDetailViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,16 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    // Do any additional setup after loading the view.
-    
 }
-
-
-
-//----------------------------------------------------------------------------------------------------------
-// Xcode
-//----------------------------------------------------------------------------------------------------------
 
 - (void)didReceiveMemoryWarning
 {
